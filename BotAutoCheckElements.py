742d3bf59fe14eb0f2cfb0e5256ff3121e71d5b0
DNA2711 <ducnguyen6693@gmail.com>
2023-09-13 10:35:54 +0700

    add somthing

--- BotAutoCheckElements.py
+++ BotAutoCheckElements.py
@@ -2,13 +2,12 @@ import concurrent.futures
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-from urllib.parse import urlparse
+from urllib.parse import urlparse, quote_plus
 import re
 import json
 import sys
 import time
 import traceback
-
 
 # Hàm kiểm tra các phần tử đăng nhập
 
@@ -18,7 +17,7 @@ def check_login_elements(content):
     keywords = ["sign up", "sign in", "log in", "signup", "login", "Account",
                 "subscribe", "ACCOUNT", "member", "join now", "join us", "register", "NEWSLETTER"]
     regex_pattern = re.compile(
-        r"\b(" + "|".join(re.escape(keyword) for keyword in keywords) + r")\b", re.I)
+        r"\b(" + "|".join(re.escape(keyword.replace('&', '\&')) for keyword in keywords) + r")\b", re.I)
 
     matching_elements = []  # Danh sách để lưu trữ các thẻ khớp
 
@@ -43,7 +42,71 @@ def find_affiliate_links(content):
 
     return matching_links
 
-# Hàm kiểm tra trang web cho một thương hiệu
+
+def find_AboutUs_links(content):
+    soup = BeautifulSoup(content, "html.parser")
+    AboutUs_keywords = ["About us", "ABOUT US", "About Us", "about us"]
+
+    matching_links = ''  # Danh sách để lưu trữ các liên kết liên quan đến từ khóa "About Us"
+
+    for link in soup.find_all('a', href=True):
+        href = link['href']
+        text = link.text
+        for keyword in AboutUs_keywords:
+            if keyword in text:
+                matching_links = href
+
+    return matching_links
+
+
+def find_privacy_links(content):
+    soup = BeautifulSoup(content, "html.parser")
+    privacy_keywords = ["Privacy", "privacy",
+                        "Privacy notice", "privacy notice", "Privacy Notice"]
+
+    matching_links = ''
+
+    for link in soup.find_all('a', href=True):
+        href = link['href']
+        text = link.text
+        for keyword in privacy_keywords:
+            if keyword in text:
+                matching_links = href
+
+    return matching_links
+
+
+def find_contact_links(content):
+    soup = BeautifulSoup(content, "html.parser")
+    contact_keywords = ["Contact", "contact",
+                        "Contact Us", "contact us", "Contact us"]
+
+    matching_links = ''
+
+    for link in soup.find_all('a', href=True):
+        href = link['href']
+        text = link.text
+        for keyword in contact_keywords:
+            if keyword in text:
+                matching_links = href
+
+    return matching_links
+
+
+def find_term_links(content):
+    soup = BeautifulSoup(content, "html.parser")
+    term_keywords = ["Terms", "terms", "TERMS"]
+
+    matching_links = ''
+
+    for link in soup.find_all('a', href=True):
+        href = link['href']
+        text = link.text
+        for keyword in term_keywords:
+            if keyword in text:
+                matching_links = href
+
+    return matching_links
 
 
 def check_brand(brand_name, proxy):
@@ -57,7 +120,10 @@ def check_brand(brand_name, proxy):
         # Khởi tạo trình duyệt
         driver = webdriver.Chrome(options=chrome_options)
 
-        url_link = "https://www.google.com/search?q=" + brand_name + " official website"
+        # Mã hóa brand_name thành URL an toàn
+        encoded_brand_name = quote_plus(brand_name)
+
+        url_link = f"https://www.google.com/search?q={encoded_brand_name}+official+website"
         driver.get(url_link)
 
         # Tìm trang đầu tiên trong kết quả tìm kiếm và click vào nó
@@ -73,12 +139,38 @@ def check_brand(brand_name, proxy):
         matching_elements = check_login_elements(page_after_click_html)
         if (len(matching_elements) > 0):
             link_affiliate = find_affiliate_links(page_after_click_html)
+            link_AboutUs = find_AboutUs_links(page_after_click_html)
+            link_privacy = find_privacy_links(page_after_click_html)
+            link_contact = find_contact_links(page_after_click_html)
+            link_term = find_term_links(page_after_click_html)
+
             # Kiểm tra xem liên kết affiliate có phần domain không và thêm nếu cần
             if link_affiliate and not urlparse(link_affiliate).netloc:
                 current_url = driver.current_url
-                link_affiliate = current_url + link_affiliate
+                link_affiliate = current_url.rstrip('/') + link_affiliate
+
+            if link_AboutUs and not urlparse(link_AboutUs).netloc:
+                current_url = driver.current_url
+                link_AboutUs = current_url.rstrip('/') + link_AboutUs
+
+            if link_privacy and not urlparse(link_privacy).netloc:
+                current_url = driver.current_url
+                link_privacy = current_url.rstrip('/') + link_privacy
+
+            if link_contact and not urlparse(link_contact).netloc:
+                current_url = driver.current_url
+                link_contact = current_url.rstrip('/') + link_contact
+
+            if link_term and not urlparse(link_term).netloc:
+                current_url = driver.current_url
+                link_term = current_url.rstrip('/') + link_term
+
         else:
             link_affiliate = ""
+            link_AboutUs = ""
+            link_privacy = ""
+            link_contact = ""
+            link_term = ""
 
         current_url = driver.current_url
         driver.quit()
@@ -89,7 +181,11 @@ def check_brand(brand_name, proxy):
                 "status": "true",
                 "data": {
                     "website_url": current_url,
-                    "affiliate_links": link_affiliate
+                    "affiliate_links": link_affiliate,
+                    "AboutUs_links": link_AboutUs,
+                    "privacy_links": link_privacy,
+                    "contact_links": link_contact,
+                    "term_links": link_term
                 }
             }
         else:
@@ -98,7 +194,11 @@ def check_brand(brand_name, proxy):
                 "status": "false",
                 "data": {
                     "website_url": "",
-                    "affiliate_links": ""
+                    "affiliate_links": "",
+                    "AboutUs_links": "",
+                    "privacy_links": "",
+                    "contact_links": "",
+                    "term_links": ""
                 }
             }
 
@@ -119,7 +219,7 @@ def check_brand(brand_name, proxy):
 def main():
     try:
         input_array = sys.argv[1:]
-        # input_array = ['neverwinter']
+        # input_array = ['youn-beauty']
         result_array = []
 
         proxy_list = [
@@ -159,6 +259,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-# made by DNA
